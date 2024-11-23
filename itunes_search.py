538fa5da02e38a174b02ee7760061612720420c7
laurenchin8 <123130930+laurenchin8@users.noreply.github.com>
2024-11-23 22:47:33 +0000

    finished itunes api

--- itunes_search.py
+++ itunes_search.py
@@ -1,12 +1,14 @@
 import requests # a lib for making http requests
 import json # a lib for qorking with json
 
-url = "https://itunes.apple.com/search?media=movie&entity=movie&attribute=movieTerm&term=Thor"
+url = "https://itunes.apple.com/search?media=movie&entity=movie&term=Thor"
 
 # iTunes documentation
 # media: type of media to search for (ie: movie)
 # entity: type of results you want for media specified (ie: movie (for movie title))
-# attribute + term: attribute you want to search for
+# attribute: attribute you want to search for in the stores (ie: movieTerm)
+# term: URL-encoded text string you want to search for
+# country: default is US
 response = requests.get(url=url)
 
 # first thing, check the response's status_code
@@ -15,6 +17,21 @@ if response.status_code == 200:
     # status OK
     # we can extract the prediction from the response's JSON text
     json_object = json.loads(response.text)
-    print(json_object)
-    # prediction = json_object["prediction"]
-    # print("prediction:", prediction)
+    # get list of movies
+    movies = json_object["results"]
+    # iterate through each movie in the results
+    for movie in movies:
+        movie_name = movie["trackName"] # Movie name
+        duration_in_seconds = movie["trackTimeMillis"] / 1000  # Duration in seconds
+        
+        # convert duration to hours, minutes, and seconds
+        hours = int(duration_in_seconds / 3600)
+        min = int((duration_in_seconds % 3600) / 60)
+        sec = int(duration_in_seconds % 60)
+        
+        # print movie name and duration
+        print("Movie:", movie_name)
+        print(f"Duration: {hours} hours, {min} minutes, {sec} seconds")
+        print("\n")
+else:
+    print(f"Error: Unable to fetch data (status code {response.status_code})")
